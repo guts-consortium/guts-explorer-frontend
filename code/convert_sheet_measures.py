b53942720a3d9ea54fcb69a351cb542d0fffc9fe
Stephan Heunis <s.heunis@fz-juelich.de>
2023-02-11 23:56:39 +0100

    v1 of graph filtering option

--- code/convert_sheet_measures.py
+++ code/convert_sheet_measures.py
@@ -6,24 +6,29 @@ import sys
 
 def parse_cohort_waves(measure_entry, cohort, n_resps):
     """"""
-    # print(f"type: {type(measure_entry[cohort])}")
+    # First create a list of 
+    # We are working with string values. If value in ecc or mcc column is not a string:
     if isinstance(measure_entry[cohort], (int, float)):
         if measure_entry[cohort] == 0:
-            items = []
+            respondent_waves = []
         else:
-            items = [str(measure_entry[cohort])]
+            respondent_waves = [str(measure_entry[cohort])]
         n_items = 1
     else:
-        items = [m.strip() for m in measure_entry[cohort].split(';')]
-        n_items = len(items)
-    # print(f"items: {items}")
-    # print(f"n_resps: {n_resps}")
-    # print(f"n_items: {n_items}")
+        respondent_waves = [m.strip() for m in measure_entry[cohort].split(';')]
+        n_items = len(respondent_waves)
     assert n_items == n_resps
     waves_key = cohort + '_waves'
-    measure_entry[waves_key] = {}
-    for i,m in enumerate(items):
-        measure_entry[waves_key][measure_entry['respondents'][i]] = [el.strip() for el in m.split(',') if el.strip() != '0']
+    measure_entry[waves_key] = {
+        "all": []
+    }
+    # For the cohort, create a list of waves per respondent:
+    for i,waves in enumerate(respondent_waves):
+        parsed_waves = [el.strip() for el in waves.split(',') if el.strip() != '0']
+        measure_entry[waves_key][measure_entry['respondents'][i]] = parsed_waves
+        measure_entry[waves_key]["all"] += parsed_waves
+    # ensure unique elements in "all" list
+    measure_entry[waves_key]["all"] = list(set(measure_entry[waves_key]["all"]))
 
 
 # SCRIPT STARTS RUNNING HERE
@@ -51,29 +56,30 @@ for x,q in enumerate(overview_measures_list):
 
     # Replace NaN values with None
     new_list[x] = { k: "" if np.isreal(q[k]) and np.isnan(q[k]) else q[k] for k in q.keys()}
-    # parse respondents into separate entities
+    # parse respondents into separate elements in a list
     new_list[x]['respondents'] = [r.strip() for r in q['respondent'].split(',')]
+    # number of respondents
     n_resps = len(new_list[x]['respondents'])
-
+    # Ensure that values from ecc and mcc columns are strings
     if isinstance(new_list[x]['ecc'], (int, float)):
         new_list[x]['ecc'] = str(new_list[x]['ecc']).replace('.', ',')
-    
     if isinstance(new_list[x]['mcc'], (int, float)):
         new_list[x]['mcc'] = str(new_list[x]['mcc']).replace('.', ',')
-
-    # print(f"respondents: {new_list[x]['respondents']}")
-    # parse waves per cohort
-    # print(f"ecc waves: {new_list[x]['ecc']}")
-    # print(f"mcc waves: {new_list[x]['mcc']}")
-
+    # Write cohorts to list (if cohort contains a wave number, i.e. not zero)
+    new_list[x]['cohorts'] = []
+    waves = ["1","2","3","4","5","6","7"]
+    if any(w in new_list[x]['ecc'] for w in waves):
+        new_list[x]['cohorts'].append('ecc')
+    if any(w in new_list[x]['mcc'] for w in waves):
+        new_list[x]['cohorts'].append('mcc')
+    # Parse all the rest
     parse_cohort_waves(new_list[x], 'ecc', n_resps)
     parse_cohort_waves(new_list[x], 'mcc', n_resps)
-    # parse dois
+    # Parse DOIs
     if isinstance(q['doi'], str):
         new_list[x]['doi'] = [d.strip() for d in q['doi'].split(';')]
     else:
         new_list[x]['doi'] = []
-
 
 # Lastly Write list of dictionaries to file
 package_path = Path(__file__).resolve().parent.parent
