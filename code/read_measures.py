477850e57d7a4190139bd306579331a6944aee69
Stephan Heunis <s.heunis@fz-juelich.de>
2024-02-06 06:53:01 +0100

    data update; add script to generate participant-measure samples from participant data

--- code/read_measures.py
+++ code/read_measures.py
@@ -22,6 +22,7 @@ measure_list = measures_sheet_list.copy()
 # These fields will be removed below
 fields_to_delete = ["mapping", "cohort", "sessions", "reference", "doi"]
 
+all_measures = []
 # for each measure record:
 for x,q in enumerate(measures_sheet_list):  
     # Delete unused keys
@@ -33,6 +34,10 @@ for x,q in enumerate(measures_sheet_list):
 
     # Replace NaN values with empty string
     measure_list[x] = { k: "" if np.isreal(q[k]) and np.isnan(q[k]) else q[k] for k in q.keys()}
+    all_measures.append(measure_list[x]["short-name"])
+
+# ensure all measures are unique
+all_measures = list(set(all_measures))
 
 # Write list of dictionaries to file
 package_path = Path(__file__).resolve().parent.parent
@@ -55,3 +60,37 @@ for x,q in enumerate(participant_sheet_list):
 out_file = out_dir / 'participant_data.json'
 with open(out_file, 'w') as f:
     json.dump(ppn_list, f)
+
+
+# 3. Participant measures
+# TODO: this should perhaps rather be done in javascript code, to prevent generation
+# of a large file and having to read from a large file in the browser
+ppn_measure_list = []
+standard_fields = ["ppn", "cohort", "session", "age"]
+measure_fields = ["short-name", "data-type", "data-category"]
+for i, p in enumerate(ppn_list):
+    for m in all_measures:
+        # skip measure if it is not collected for the participant (i.e. not included in p)
+        if not p.get(m, 0):
+            continue
+        # create new participant measure with base fields from ppn record
+        ppn_measure = {
+            f: p.get(f) for f in standard_fields
+        }
+        ppn_measure["short-name"] = m
+        # get corresponding measure from short-name
+        m_dict = next((item for item in measure_list if item["short-name"] == m), None)
+        # complete short-name associated fields
+        if m_dict:
+            ppn_measure["data-type"] = m_dict.get("data-type")
+            ppn_measure["data-category"] = m_dict.get("data-category")
+        else:
+            ppn_measure["data-type"] = None
+            ppn_measure["data-category"] = None
+        # add participant measure to list
+        ppn_measure_list.append(ppn_measure)
+
+out_file = out_dir / 'participant_measures.json'
+with open(out_file, 'w') as f:
+    json.dump(ppn_measure_list, f)
+
