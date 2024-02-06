f979f4de788e6ed3b1740ddac8fcf9b7787fed9e
Stephan Heunis <s.heunis@fz-juelich.de>
2024-02-06 06:54:01 +0100

    add javascript code to load participant data and generate participant-measure samples from that; create new view to filter samples

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,10 @@ var explorer = new Vue({
         measure_data: [],
         measure_headings: [],
         measure_data_loaded: false,
+        measure_shortnames: [],
+        participant_data: [],
+        participant_measures: [],
+        participant_measures_loaded: false,
         search_text: "",
         search_text_tags: [],
     },
@@ -70,6 +74,7 @@ var explorer = new Vue({
         })
         .then((responseJson) => {
             this.measure_data = responseJson;
+            this.measure_shortnames = this.measure_data.map((m) => (m["short-name"]));
             this.measure_data_loaded = true;
             this.measure_headings = [
                 {
@@ -93,6 +98,52 @@ var explorer = new Vue({
                     label: "Category",
                 }
             ]
+            participant_data_file = 'data/participant_data.json'
+            return fetch(participant_data_file)
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log(
+                    "WARNING: participant_data.json file could not be loaded"
+                );
+            }
+        })
+        .then((responseJson) => {
+            this.participant_data = responseJson;
+            // Loop through all participants and create sample data
+            for (var i=0; i<this.participant_data.length; i++) {
+                part = this.participant_data[i]
+                // Loop through all measure shortnames
+                for (var m=0; m<this.measure_shortnames.length; m++) {
+                    m_code = this.measure_shortnames[m]
+                    // skip measure if it is not collected for the participant
+                    if (!part.hasOwnProperty(m_code) || !part[m_code]) {
+                        continue
+                    }
+                    // construct participant_measure object
+                    var part_measure = {
+                        "ppn": part["ppn"],
+                        "cohort": part["cohort"] ,
+                        "session": part["session"] ,
+                        "age": part["age"],
+                        "short-name": m_code,
+                    }
+                    // get corresponding measure from short-name
+                    var measure = this.measure_data.find(x => x["short-name"] === m_code);
+                    // complete short-name associated fields
+                    if (measure) {
+                        part_measure["data-type"] = measure["data-type"]
+                        part_measure["data-category"] = measure["data-category"]
+                    } else {
+                        part_measure["data-type"] = null
+                        part_measure["data-category"] = null
+                    }
+                    this.participant_measures.push(part_measure)
+                }
+            }
+            this.participant_measures_loaded = true;
         })
     },
 });
